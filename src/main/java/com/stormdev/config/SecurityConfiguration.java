8df0996f5390f8827b033e431f5e592da6f84637
Vinicius Ribeiro <viniribeiroa@gmail.com>
2025-08-06 22:30:46 -0300

    Spring Security

---
+++ src/main/java/com/stormdev/config/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+/**
+ * Autor: VINICIUS
+ * Data: 6 de ago. de 2025
+ * Descrição: TODO
+ */
+package com.stormdev.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults())
+				.authorizeHttpRequests(authorize -> {
+					authorize.anyRequest().authenticated();
+				})
+				.build();
+	}
+}
--- src/main/java/com/stormdev/controller/LivroController.java
+++ src/main/java/com/stormdev/controller/LivroController.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,7 +61,7 @@ public class LivroController implements GenericController{
 				}).orElseGet(() -> ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("{id}")
+	@DeleteMapping("{id}")
 	public ResponseEntity<Object> deletar(@PathVariable("id")String id){
 		return service.obterPorId(UUID.fromString(id))
 				.map(livro -> {
