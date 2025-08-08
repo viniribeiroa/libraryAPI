3ea22bcf76db0862a8188b1aebf7034dd29d0076
Vinicius Ribeiro <viniribeiroa@gmail.com>
2025-08-07 22:24:08 -0300

    repository de usuario em memoria

--- src/main/java/com/stormdev/config/SecurityConfiguration.java
+++ src/main/java/com/stormdev/config/SecurityConfiguration.java
@@ -11,6 +11,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -32,5 +38,29 @@ public class SecurityConfiguration {
 					authorize.anyRequest().authenticated();
 				})
 				.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10);
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		
+		
+		UserDetails user1 = User.builder()
+				.username("usuario")
+				.password(encoder.encode("1234"))
+				.roles("USER")
+				.build();
+		
+		UserDetails user2 = User.builder()
+				.username("admin")
+				.password(encoder.encode("1234"))
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user1, user2);
 	}
 }
