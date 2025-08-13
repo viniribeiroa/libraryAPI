27e2c20d57e42642b714d79bb365cdac66a20d93
Vinicius Ribeiro <viniribeiroa@gmail.com>
2025-08-12 22:06:49 -0300

    custom Authetication

---
+++ src/main/java/com/stormdev/security/CustomAuthetication.java
@@ -0,0 +1,74 @@
+/**
+ * Autor: VINICIUS
+ * Data: 12 de ago. de 2025
+ * Descrição: TODO
+ */
+package com.stormdev.security;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.stormdev.model.Usuario;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 
+ */
+@RequiredArgsConstructor
+@Getter
+public class CustomAuthetication implements Authentication{
+
+	private final Usuario usuario;
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return usuario.getLogin();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return this.usuario
+				.getRoles()
+				.stream().map(role -> new SimpleGrantedAuthority(role))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Object getCredentials() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getDetails() {
+		// TODO Auto-generated method stub
+		return usuario;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		// TODO Auto-generated method stub
+		return usuario;
+	}
+
+	@Override
+	public boolean isAuthenticated() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
