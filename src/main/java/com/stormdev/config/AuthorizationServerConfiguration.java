25f5c4ee35c585650fd9a6a87e5c9bb80844fc5a
Vinicius Ribeiro <viniribeiroa@gmail.com>
2025-09-01 22:06:30 -0300

    configuração de tokens JWK e JWT

--- src/main/java/com/stormdev/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/stormdev/config/AuthorizationServerConfiguration.java
@@ -5,7 +5,12 @@
  */
 package com.stormdev.config;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +20,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.web.authentication.PublicClientAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 /**
  * 
@@ -62,6 +75,37 @@ public class AuthorizationServerConfiguration {
 		return ClientSettings.builder()
 				.requireAuthorizationConsent(false)
 				.build();
+	}
+	
+	@Bean
+	public JWKSource<SecurityContext> jwkSource() throws Exception{
+		
+		RSAKey rsaKey = gerarChaveRSA();
+		
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	
+	//Gerar par de chaves RSA
+	private RSAKey gerarChaveRSA() throws Exception{
+		
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(2048);
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		
+		RSAPublicKey chavePublica = (RSAPublicKey)keyPair.getPublic();
+		RSAPrivateKey chavePrivada = (RSAPrivateKey)keyPair.getPrivate();
+		
+ 		return new RSAKey.Builder(chavePublica)
+ 				.privateKey(chavePrivada)
+ 				.keyID(UUID.randomUUID().toString())
+ 				.build();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
 	}
 
 }
