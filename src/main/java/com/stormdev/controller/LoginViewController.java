3ddb2c26b5dfa53fd28818465247a10b79f74aa0
Vinicius Ribeiro <viniribeiroa@gmail.com>
2025-09-02 21:40:01 -0300

    authorized

--- src/main/java/com/stormdev/controller/LoginViewController.java
+++ src/main/java/com/stormdev/controller/LoginViewController.java
@@ -8,6 +8,7 @@ package com.stormdev.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.stormdev.security.CustomAuthetication;
@@ -30,6 +31,12 @@ public class LoginViewController {
 			System.out.println(customAuth.getUsuario());
 		}
 		return "Olá " + authentication.getName();
+	}
+	
+	@GetMapping("/authorized")
+	@ResponseBody
+	public String getAuthorizationCode(@RequestParam("code") String code) {
+		return "Seu authorization code: " + code;
 	}
 
 }
